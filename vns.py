8e811332387c9928239e8a0a4aaaf3a8b0cca70f
songjnli <149924310+songjnli@users.noreply.github.com>
2023-11-14 20:10:06 +0800

    Update vns.py

--- vns.py
+++ vns.py
@@ -1,7 +1,45 @@
 from Sol import *
 
+def penalty_route(r, vehicle_type=1, depart_time=0):
+    """
+    包含两部分的内容：违反时间约束的惩罚成本（单位：分钟）+违反电量约束的惩罚成本（单位：百米）
+    要加上所有的cost！
+    :return: 惩罚成本
+    """
+    penalty_time = 0
+    penalty_elec = 0
+    charge = df_vehicle.loc[vehicle_type, 'driving_range']
+    res_charge = charge
+    path_time = depart_time
 
-def cost_route(r, vehicle_type=1, depart_time=0, penalty=False, penalty_lam=0):
+    for j in range(len(r) - 1):
+        from_point = r[j]
+        to_point = r[j+1]
+
+        path_time += df_distance.loc[(from_point, to_point), 'spend_tm']
+        if to_point != 0:
+            penalty_time += max((path_time - df_nodes.loc[to_point, 'last_receive_tm']), 0)  # 晚到惩罚，单位：分钟
+        penalty_time += max((path_time - time_horizon), 0)  # 所有点超过time_horizon加一个额外的惩罚
+        if to_point != 0:  # 更新时间参数
+            path_time = max(df_nodes.loc[to_point, 'first_receive_tm'], path_time)
+        if to_point == 0:
+            path_time += 60
+        else:
+            path_time += service_time
+
+        res_charge = res_charge - df_distance.loc[(from_point, to_point), 'distance']
+        if res_charge < 0:
+            penalty_elec += -res_charge / 100  # 电量惩罚，单位：百米
+        if to_point in index_recharge:  # 更新电量参数
+            res_charge = charge
+        if to_point == 0:  # 回depot也可以充满的
+            res_charge = charge
+
+    total_penalty_cost = penalty_time + penalty_elec
+
+    return total_penalty_cost
+
+def cost_route(r, vehicle_type=1, depart_time=0):
     """
     计算单独一条路的cost。包含
     1. 车辆固定成本
@@ -53,6 +91,31 @@ def cost_route(r, vehicle_type=1, depart_time=0, penalty=False, penalty_lam=0):
 
     return total_cost
 
+def penalty_cost_route(r, vehicle_type=1, depart_time=0, penalty=False, penalty_lam=0):
+    """
+    计算单独一条路的cost。包含
+    1. 车辆固定成本
+    2. travel cost
+    3. waiting cost
+    4. charging cost
+    :param r: 如[0, 1, 0]
+    :param vehicle_type: 1或2,默认为1
+    :param depart_time: 出发时间,默认为0
+    :return: 这条路的成本
+    TODO: 重新实现目标函数，加入惩罚项
+    """
+    total_cost_route = 0
+    total_penalty_route = 0
+    total_penalty_cost = 0
+
+    total_cost_route = cost_route(r, vehicle_type, depart_time)
+
+    if penalty == True:
+        total_penalty_route = penalty_route(r, vehicle_type, depart_time)
+
+    total_penalty_cost += total_cost_route + penalty_lam * total_penalty_route
+
+    return total_penalty_cost
 
 def labeling(r, vehicle_type, departure_time):
     """
