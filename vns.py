978a2d3b4a29eef5ca84926450631598fbe0545d
shi-ym19 <72424629+shi-ym19@users.noreply.github.com>
2023-11-14 21:45:25 +0800

    Add files via upload

--- vns.py
+++ vns.py
@@ -1,45 +1,7 @@
 from Sol import *
 
-def penalty_route(r, vehicle_type=1, depart_time=0):
-    """
-    包含两部分的内容：违反时间约束的惩罚成本（单位：分钟）+违反电量约束的惩罚成本（单位：百米）
-    要加上所有的cost！
-    :return: 惩罚成本
-    """
-    penalty_time = 0
-    penalty_elec = 0
-    charge = df_vehicle.loc[vehicle_type, 'driving_range']
-    res_charge = charge
-    path_time = depart_time
 
-    for j in range(len(r) - 1):
-        from_point = r[j]
-        to_point = r[j+1]
-
-        path_time += df_distance.loc[(from_point, to_point), 'spend_tm']
-        if to_point >= 1 and to_point <= 1000:
-            penalty_time += max((path_time - df_nodes.loc[to_point, 'last_receive_tm']), 0)  # 晚到惩罚，单位：分钟
-        penalty_time += max((path_time - time_horizon), 0)  # 所有点超过time_horizon加一个额外的惩罚
-        if to_point != 0:  # 更新时间参数
-            path_time = max(df_nodes.loc[to_point, 'first_receive_tm'], path_time)
-        if to_point == 0:
-            path_time += 60
-        else:
-            path_time += service_time
-
-        res_charge = res_charge - df_distance.loc[(from_point, to_point), 'distance']
-        if res_charge < 0:
-            penalty_elec += -res_charge / 100  # 电量惩罚，单位：百米
-        if to_point in index_recharge:  # 更新电量参数
-            res_charge = charge
-        if to_point == 0:  # 回depot也可以充满的
-            res_charge = charge
-
-    total_penalty_cost = penalty_time + penalty_elec
-
-    return total_penalty_cost
-
-def cost_route(r, vehicle_type=1, depart_time=0):
+def cost_route(r, vehicle_type=1, depart_time=0, penalty=False, penalty_lam=0):
     """
     计算单独一条路的cost。包含
     1. 车辆固定成本
@@ -91,31 +53,6 @@ def cost_route(r, vehicle_type=1, depart_time=0):
 
     return total_cost
 
-def penalty_cost_route(r, vehicle_type=1, depart_time=0, penalty=False, penalty_lam=0):
-    """
-    计算单独一条路的cost。包含
-    1. 车辆固定成本
-    2. travel cost
-    3. waiting cost
-    4. charging cost
-    :param r: 如[0, 1, 0]
-    :param vehicle_type: 1或2,默认为1
-    :param depart_time: 出发时间,默认为0
-    :return: 这条路的成本
-    TODO: 重新实现目标函数，加入惩罚项
-    """
-    total_cost_route = 0
-    total_penalty_route = 0
-    total_penalty_cost = 0
-
-    total_cost_route = cost_route(r, vehicle_type, depart_time)
-
-    if penalty == True:
-        total_penalty_route = penalty_route(r, vehicle_type, depart_time)
-
-    total_penalty_cost += total_cost_route + penalty_lam * total_penalty_route
-
-    return total_penalty_cost
 
 def labeling(r, vehicle_type, departure_time):
     """
@@ -125,6 +62,44 @@ def labeling(r, vehicle_type, departure_time):
     :param departure_time: r原本的出发时间
     :return: r_ret为返回的路径（决定了充电站、返回配送中心）, vehicle_type_ret为决定的配送车型
     """
+    
+    R = []
+    W = []
+    V = []
+    T = []
+    T_w = []
+    d = []
+    f = []
+    R[0] = 0
+    W[0] = 0
+    V[0] = 0
+    T[0] = departure_time
+    T_w[0] = 0
+    d[0] = 0
+    f[0] = 0
+    for i in range(len(r)):
+        if i != 0:
+            premise1 = (T[i]>=service_time) and (T[i]<=service_time+df_nodes.loc[r[i], "last_receive_tm"])
+            premise2 = (W[i]<=df_vehicle.loc[vehicle_type, "max_weight"])
+            premise3 = (V[i]<=df_vehicle.loc[vehicle_type, "max_volume"])
+            premise4 = R[i] <= df_vehicle.loc[vehicle_type, "driving_range"]
+            premise = premise1 and premise2 and premise3 and premise4
+            if not premise:
+                continue
+            
+            extension = 1
+            #### 拓展1
+            if extension == 1:
+                T[i+1] = max(T[i]+df_distance.loc[(r[i], r[i+1]), "spend_tm"], df_nodes.loc[r[i], "first_receive_tm"])+service_time
+                T_w[i+1] = T_w[i] + max(0, df_nodes.loc[r[i], "first_receive_tm"]-T[i]-df_nodes.loc[(r[i], r[i+1]), "spend_tm"])
+                W[i+1] = W[i] + df_nodes.loc[r[i+1], "pack_total_weight"])
+                V[i+1] = V[i] + df_nodes.loc[r[i+1], "pack_total_volume"])    
+                R[i+1] = R[i] + df_distance.loc[(r[i], r[i+1]), "distance"]
+                d[i+1] = d[i] + df_distance.loc[(r[i], r[i+1]), "distance"]
+                time_wait = max(0, df_nodes.loc[r[i], "first_receive_tm"]-T[i]-df_nodes.loc[(r[i], r[i+1]))
+                f[i+1] = f[i]+waiting_cost*time_wait+df_distance.loc[(from_point, to_point), 'distance'] * df_vehicle.loc[vehicle_types, 'unit_trans_cost'] / 1000
+                
+                
     r_ret = r
     vehicle_type_ret = vehicle_type
     return r_ret, vehicle_type_ret
